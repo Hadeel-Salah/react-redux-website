dc93202c9344ce4ae7f743d34f137cbea2ebb974
Hadeel Salah <42890131+hadeelsala7@users.noreply.github.com>
2019-12-14 01:41:02 +0200

    Update index.js from reducer-branch

--- movies/src/reducers/index.js
+++ movies/src/reducers/index.js
@@ -1,4 +1,5 @@
 // in this file , combine all reducer in one reducer and set it into single parent state
+//
 
 import { combineReducers } from 'redux';
 import searchReducer from './searchReducer';
